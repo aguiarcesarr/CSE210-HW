17400d596735e89d3448c138ba1be0fdc7058846
aguiarcesarr <cesarbras2020@gmail.com>
2025-05-11 16:54:44 -0300

    Added

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This program asks the user for their first and last name and then greets them.
         Console.Write("What is your first name?.");
        
         string firstName = Console.ReadLine();
